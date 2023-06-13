11046fbb8dda585c19eb555fcf9a602da4af311c
vlado.markovic <vmarkovic477@gmail.com>
2023-06-13 02:27:32 +0200

    requests task done

--- tasks/useri_i_postovi.py
+++ tasks/useri_i_postovi.py
@@ -30,17 +30,31 @@ def get_user_posts(url):
     if response.status_code == 200:
         return response
     else:
-        print('GETsssss zahtjev neuspjesan, status kod: ', response.status_code)
+        print('GET zahtjev neuspjesan, status kod: ', response.status_code)
 
+# get users and posts
 users = users_request(base_url).json()
 posts = get_user_posts(base_url).json()
 
-dict_posts = {d['id']: d for d in posts}
+# turn users into dictionary with id as pk
+users_dict = {user['id']: user for user in users}
 
-combined = [{**item1, **dict_posts.get(item1['id'], {})} for item1 in users]
 
+# iterate over posts, match id=userId, and create new dict in list
+combined = [
+    {
+        "id": users_dict[post['userId']]['id'],
+        "name": users_dict[post['userId']]['name'],
+        "username": users_dict[post['userId']]['username'],
+        "email": users_dict[post['userId']]['email'],
+        "post_id": post['id'],
+        "post_title": post['title'],
+        "post_body": post['body'],
+    }
+    for post in posts if post['userId'] in users_dict
+]
 
-# Write to JSON file
-with open('output.json', 'w') as f:
+# write to JSON file
+with open('users-posts.json', 'w') as f:
     json.dump(combined, f)
     
